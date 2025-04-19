5b7d0439c23060753d1e289a59d4efc1dcea4d63
Atay <ataytenalievkanatovich@gmail.com>
2025-04-19 11:46:43 +0600

    Скрол

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,34 +7,27 @@ const Footer = () => {
         <div className="footer__inner">
           <div className="footer__block footer__block--left">
             <img src="/Logo.png" alt="Logo" className="footer__logo" />
-            <a href="#" className="footer__link footer__link--big">О нас</a>
+            <a href="#" className="footer__link footer__link--big">криптовалютная платформа для управления инвестициями.</a>
           </div>
 
-          {/* Центр: 4 ссылки */}
           <div className="footer__block footer__block--center">
-            <div className="footer__links">
-              <a href="#" className="footer__link footer__link--small">Услуги</a>
-              <a href="#" className="footer__link footer__link--small">Проекты</a>
-            </div>
-            <div className="footer__links">
-              <a href="#" className="footer__link footer__link--small">Команда</a>
-              <a href="#" className="footer__link footer__link--small">Контакты</a>
+            <div className="footer__link-grid">
+              <a href="#" className="footer__link footer__link--small" style={{ width: '150px' }}>Публичный трейдинг</a>
+              <a href="#" className="footer__link footer__link--small" style={{ width: '153px' }}>Доходы и безопасность</a>
+              <a href="#" className="footer__link footer__link--small" style={{ width: '163px' }}>Пассивный заработок</a>
+              <a href="#" className="footer__link footer__link--small" style={{ width: '109px' }}>Обратная связь</a>
             </div>
           </div>
 
-          {/* Иконки */}
-
+          {/* Справа: 4 иконки + 1 ссылка под ними */}
           <div className="footer__block footer__block--right">
-            <div className="footer__block footer__block--icons">
-              <div className="footer__icons-container">
-                <img src="/Vector.svg" alt="icon1" className="footer__icon footer__icon--large" />
-                <img src="/ctor.svg" alt="icon2" className="footer__icon footer__icon--small" />
-                <img src="/pos.svg" alt="icon3" className="footer__icon footer__icon--small" />
-                <img src="/Whatsapp.svg" alt="icon4" className="footer__icon footer__icon--small" />
-              </div>
+            <div className="footer__icons-container">
+              <a href="#"><img src="/Vector.svg" alt="icon1" className="footer__icon footer__icon--large" /></a>
+              <a href="#"><img src="/ctor.svg" alt="icon2" className="footer__icon footer__icon--small" /></a>
+              <a href="#"><img src="/pos.svg" alt="icon3" className="footer__icon footer__icon--small" /></a>
+              <a href="#"><img src="/Whatsapp.svg" alt="icon4" className="footer__icon footer__icon--small" /></a>
             </div>
-
-            <a href="#" className="footer__link footer__link--big">Поддержка</a>
+            <a href="#" className="footer__link footer__link--under-icons">Политика конфиденциальности</a>
           </div>
         </div>
       </footer>
@@ -42,3 +35,4 @@ const Footer = () => {
 };
 
 export default Footer;
+
