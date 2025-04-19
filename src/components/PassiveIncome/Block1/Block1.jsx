9b0be89ad1d21f99b0f739a5dcdc3322dda4fad6
nurhan004 <sharshenbekov777@gmail.com>
2025-04-19 15:58:58 +0600

    add

--- src/components/PassiveIncome/Block1/Block1.jsx
+++ src/components/PassiveIncome/Block1/Block1.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
-import "./Block1.scss"
-import img1 from "./svg/Frame (6).svg"
-import img2 from "./svg/Frame (7).svg"
+import React from 'react';
+import "./Block1.scss";
+import img1 from "./svg/Frame (6).svg";
+import img2 from "./svg/Frame (7).svg";
 
 function Block1() {
   return (
-    <div>
-        <div  className='hand'>
-            <div>
-            <img src={img1} alt="" />
-            </div>
-            <div>
-                <h1>Торгуй на своей бирже, попади в список топ-трейдеров и собирай платных подписчиков со всего мира. </h1>
-                <div className='BlueLine'></div>
-                <p>Больше не нужно заводить Телеграм-канал для публикации сигналов и тратить огромные деньги на рекламу. Мы сами привлекаем аудиторию и автоматизируем процесс взаимодействия с подписчиками. Вам остается только сфокусироваться на торговле.</p>
-            </div>
+    <div className="feature-blocks">
+      <div className="feature-block">
+        <div className="feature-block__image">
+          <img src={img1} alt="Торговля на бирже" />
         </div>
-        <div  className='hand'>
-            <div>
-                <h1>Все твои сделки будут автоматически открываться у твоих подписчиков. </h1>
-                <div className='BlueLine'></div>
-                <p>Процент от депозита, кредитное плечо, тейки и стопы будут сразу расставлены в их сделках. Благодаря этому они сэкономят кучу времени для вхождения в сделку, не будут задавать лишних вопросов и не смогут отклониться от вашего риск-менеджмента</p>
-            </div>
-            <div>
-            <img src={img2} alt="" />
-            </div>
+        <div className="feature-block__content">
+          <h2>Торгуй на своей бирже, попади в список топтрейдеров и собирай платных подписчиков со всего мира.</h2>
+          <div className="divider"></div>
+          <p>Больше не нужно выходить. Товарищи знаем один кубиковый состав: «Теперь сделаем деньги на вопрос». Им нельзя привлечь внимание к вашим командирум, так как их пользование заканчивается. Мы сможем только фокусироваться на хорошие подписания.</p>
         </div>
+      </div>
+      
+      <div className="feature-block feature-block--reversed">
+        <div className="feature-block__content">
+          <h2>Все твои сделки будут автоматически открываться у твоих подписчиков.</h2>
+          <div className="divider"></div>
+          <p>Процент от деловых, выделённых здесь, тайны и стены будут сразу расставаться в поселках. Благодаря чему они остановят луч времени для внимания в кабину, могут задавать позднее вопросы и не смогут становиться от вашего рекомендациона.</p>
+        </div>
+        <div className="feature-block__image">
+          <img src={img2} alt="Автоматические сделки" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Block1
+export default Block1;
--- src/components/PassiveIncome/PassiveIncome.jsx
+++ src/components/PassiveIncome/PassiveIncome.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import "./PassiveIncome.scss"
-import Block1 from './Block1/Block1'
-import HowDoesThisWork from './HowDoesThisWork/HowDoesThisWork'
+import React from 'react';
+import "./PassiveIncome.scss";
+import Block1 from './Block1/Block1';
+import HowDoesThisWork from './HowDoesThisWork/HowDoesThisWork';
+import VideoBlock from './VideoBlock/VideoBlock';
 
 function PassiveIncome() {
   return (
-    <section className='PassiveIncomeContainer'>
-      <h1 className='PassiveIncomeText'>Публичный трейдинг</h1>
-      <p className='PassiveIncomeParagraf'>платформа управления инвестициями на рынке криптовалют, которая позволяет частным инвесторам копировать реальные сделки успешных профессиональных трейдеров.</p>
-      <div>
-        <Block1/>
-        <HowDoesThisWork/>
+    <section className='passive-income'>
+      <div className='passive-income__container'>
+        <h1 className='passive-income__title'>Публичный трейдинг</h1>
+        <p className='passive-income__description'>
+          платформа управления инвестициями на рынке криптовалют, которая позволяет частным инвесторам копировать реальные сделки успешных профессиональных трейдеров.
+        </p>
+        <div className='passive-income__content'>
+          <Block1/>
+          <HowDoesThisWork/>
+          <VideoBlock/>
+        </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default PassiveIncome
+export default PassiveIncome;
